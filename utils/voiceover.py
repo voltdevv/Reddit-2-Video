2922e0e5791432914f56507fb523823ff9be8861
zadam <zerenadam7@gmail.com>
2025-10-08 14:05:40 -0400

    added multi post voiceover generation

--- utils/voiceover.py
+++ utils/voiceover.py
@@ -8,6 +8,8 @@ class Voice:
         self.rate = rate
         self.output_path = output_path
         self.file_name = file_name if file_name.endswith(".wav") else file_name + ".wav"
+        self.files = []
+        self.count = 0
 
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
@@ -23,6 +25,7 @@ class Voice:
 
         steps = [
             lambda: engine.setProperty("rate", self.rate),
+            lambda: self.__check(),
             lambda: engine.save_to_file(text, self.get_full_path()),
             lambda: engine.runAndWait(),
             lambda: engine.stop()
@@ -33,3 +36,12 @@ class Voice:
 
         print(f"[✔] Audio saved to: {self.get_full_path()}")
 
+    
+    def __check(self):
+        
+        while self.file_name in self.files:
+            name, ext = os.path.splitext(self.file_name)
+            self.file_name = name + str(self.count) + ext
+            self.count += 1
+
+        self.files.append(self.file_name)
