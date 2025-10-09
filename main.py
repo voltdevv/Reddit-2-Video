9fc8fc44520b3fe8fcb5b531631d937604a4f49a
zadam <zerenadam7@gmail.com>
2025-10-08 20:43:01 -0400

    this too much bruh

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
+from concurrent.futures import ThreadPoolExecutor
 from utils.story import GetPosts
 from utils.voiceover import Voice
 from utils.filter import filter
 from utils.subtitles import segment
 from utils.edit import Editor
 
-
-
-post = GetPosts('https://www.reddit.com/r/AITAH/', 3)[1].text
-
-v = Voice()
-video = 'resources/video/video.mp4'
-
-audio_file = v.generate(filter(post))
-subtitles = segment(audio_file)
-
-e = Editor(video, audio_file)
-
-
-e.add_subtitles(subtitles)
-e.set_video_duration(3)
-e.complete()
+# ----------- Config -----------
+REDDIT_URL = ''
+NUMBER_OF_POSTS = 1 # 25 max
+# ------------------------------
